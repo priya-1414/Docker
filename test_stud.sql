80c1a37bef62aa50d54ebedf3e84813018bf7004
Priyanka <priya-1414>
2021-03-16 23:43:48 +0530

    First commit Message

---
+++ test_stud.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE studentdata
+{
+    Roll_no       integer  PRIMARY KEY,
+    stud_name     varchar(32)      ,
+    address       varchar(64)      ,
+    mobile_no     varchar(16)   ,
+    PAN_no        varchar(20),
+};
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(1,'Priyanka','partur','2374567730','abc4653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(2,'Saloni','Aurangabad','6839913630','egfd094653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(3,'Vaishnavi','Buldhana','9856628630','xyz90653');
+
+insert into studentdata(Roll_no,stud_name,address ,mobile_no, PAN_no )values(4,'Sonali','Ahmednagar','8885581530','uvz70846');
